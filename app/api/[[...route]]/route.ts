2b6e9dd5263ce977277f65bbe6b88b58c67bfa22
Olle Mineur <olle.mineur@gmail.com>
2024-03-21 00:23:52 +0100

    API routes now works, but no data is served from backend

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,9 +1,10 @@
+import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
-import { handle } from '@hono/node-server/vercel'
+//import { handle } from '@hono/node-server/vercel'
 
 export const runtime = 'nodejs';
 
-const app = new Hono().basePath('/api')
+const app = new Hono({ strict: false }).basePath('/api')
 
 app.get('/hello', async (c) => {
     return c.json({
@@ -11,5 +12,22 @@ app.get('/hello', async (c) => {
     });
 });
 
-export const GET = handle(app)
+app.notFound((c) => {
+    return c.text('Något gick verkligen fel 404', 404)
+})
+
+app.onError((err, c) => {
+    console.error(`${err}`)
+    return c.text('Något gick fel', 500)
+})
+
+// TODO: Fix why it needs different port
+export const GET = serve({
+    fetch: app.fetch,
+    port: 3500,
+    hostname: 'localhost',
+    //https: true,
+})
+
+//export const GET = handle(app)
 //export const POST = handle(app)
